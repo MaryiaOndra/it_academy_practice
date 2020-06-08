849d31aaebc1544e831e2a4e2a5fd31b6ae646a7
Mary Ondra <vidra48@mail.ru>
2020-06-08 21:01:37 +0300

    can't do it in time

---
+++ Practice/GoodDay/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GoodDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        
+    }
+
+    class Greeting
+    {
+        DateTime Morning;
+        realTime = DateTime.Now.TimeOfDay;
+            System.TimeSpan
+        DateTime Evening;
+        DateTime Night;
+        DateTime.Now {}
+        TimeSpan
+
+    }
+}
---
+++ Practice/LettersCount/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LettersCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputWord = Console.ReadLine();
+            int lenghtString = inputWord.Length;
+          
+
+           
+
+        }
+    }
+}
