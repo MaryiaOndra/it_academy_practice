0d6499f590136811acf41f011c8c89d64a0be0f8
ylia7 <ylia7@DESKTOP-F68RT3J>
2020-06-10 23:46:02 +0300

    Task2: Changed Task2

--- Practice/Lecture.6/Program.cs
+++ Practice/Lecture.6/Program.cs
@@ -6,46 +6,55 @@ namespace Lecture._6
     {
         static void Main(string[] args)
         {
-            Task1();
+            //Task1();
             Task2();
-            Task3();
-            Task4();
+            //Task3();
+            //Task4();
 
-            void Task1()
-            {
-                object[] empty = new object[default];
-            }
+            //void Task1()
+            //{
+            //    object[] empty = new object[default];
+            //}
 
-           void Task2()
+            void Task2()
             {
                 object[] newArray = { 32, 'A', "Hello" };
-                Console.WriteLine(newArray);
+
                 for (int i = 0; i < newArray.Length; i++)
                 {
-                    newArray[0] = i + 10;
-                    newArray[3] = i + " , guys";
-                }             
+                    Console.WriteLine(newArray[i]);
+                }
 
-
-            }
-
-            static void Task3()
-            {
-                int rend = 0;
-                int[] randomArray = new[] { };
-                for (int i = 0; i < length; i++)
+                for (int i = 0; i < newArray.Length; i++ )
                 {
+                    string guys = " ,guys";
+                    newArray[0] = Convert.ToInt32(newArray[0]) + 10;
+                    newArray[2] = Convert.ToString(newArray[2]) + guys;
+                    Console.WriteLine(newArray[i]);
+                }
 
-                }       
+                Console.ReadLine();
+
+
             }
-            
-            static void Task4()
-            {
-                short t = 5;
-                object o = t;
-                sbyte c = (sbyte)o;
-                Console.WriteLine(c);
-            }            
+
+            //static void Task3()
+            //{
+            //    int rend = 0;
+            //    int[] randomArray = new[] { };
+            //    for (int i = 0; i < length; i++)
+            //    {
+
+            //    }
+            //}
+
+            //static void Task4()
+            //{
+            //    short t = 5;
+            //    object o = t;
+            //    short sbyte c = (short)o;
+            //    Console.WriteLine(c);
+            //}            
         }
     }
 }
