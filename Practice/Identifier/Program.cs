c130e7ea0902f800711fb991ac8f0a9a41fd5596
Mary Ondra <vidra48@mail.ru>
2020-07-06 20:10:11 +0300

    added files

---
+++ Practice/Identifier/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Identifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UniqueItem uniqueItem1 = new UniqueItem();
+            UniqueItem uniqueItem2 = new UniqueItem();
+            UniqueItem uniqueItem3 = new UniqueItem();
+
+            UniqueItem uniqueItem4 = new UniqueItem(50);
+            UniqueItem uniqueItem5 = new UniqueItem();
+            UniqueItem uniqueItem6 = new UniqueItem();
+        }
+    }
+}
---
+++ Practice/Identifier/UniqueItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Identifier
+{
+    class UniqueItem
+    {
+        static private int _startID;
+        static private int itemID;
+        static private int count;
+
+        public UniqueItem()
+        {
+            count++;
+            _startID = count;
+        }
+        public UniqueItem(int startID)
+        {
+            _startID = startID;
+            count++;             
+        }
+    }
+}
